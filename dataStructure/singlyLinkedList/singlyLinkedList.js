aac27f4db0c910a127638600a62eb907c61b513e
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-06-21 23:49:29 -0400

    SinglyLinkedList: unshift method

--- dataStructure/singlyLinkedList/singlyLinkedList.js
+++ dataStructure/singlyLinkedList/singlyLinkedList.js
@@ -51,6 +51,19 @@ class SinglyLinkedList {
     }
     return currentHead;
   }
+
+  unshift(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 const list = new SinglyLinkedList();
 // For a better test, use the browser
