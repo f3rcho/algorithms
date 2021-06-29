a6a0158621998f7cc8b4f2c9f550d08b666432f2
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-06-28 23:53:41 -0400

    BinarySearchTree: find Method

--- dataStructure/binarySearchTree/bst.js
+++ dataStructure/binarySearchTree/bst.js
@@ -38,6 +38,22 @@ class BinarySearchTree {
       }
     }
   }
+  find(value) {
+    if (this.root === null) return false;
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return false;
+    return current;
+  }
 }
 
 let tree = new BinarySearchTree();
