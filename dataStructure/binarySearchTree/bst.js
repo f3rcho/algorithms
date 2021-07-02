6d828f73d615dcc6df65c9d1e964334a60e683b5
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-07-02 15:17:57 -0400

    Breadth First Search

--- dataStructure/binarySearchTree/bst.js
+++ dataStructure/binarySearchTree/bst.js
@@ -54,13 +54,26 @@ class BinarySearchTree {
     if (!found) return false;
     return current;
   }
+  BFS() {
+    let node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
 }
 
 let tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
