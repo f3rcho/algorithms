1f78336595230ccd228aafdf43cd12676c4557fa
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-07-07 11:46:33 -0400

    HasTable: keys and values method

--- dataStructure/hashTable/hashTable.js
+++ dataStructure/hashTable/hashTable.js
@@ -33,6 +33,33 @@ class HashTable {
     }
     return undefined;
   }
+
+  values() {
+    let valuesArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (j = 0; j < this.keyMap[i].length; j++) {
+          if (!valuesArr.includes(this.keyMap[i][j][1])) {
+            valuesArr.push(this.keyMap[i][j][1]);
+          }
+        }
+      }
+    }
+    return valuesArr;
+  }
+  keys() {
+    let keysArr = [];
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (j = 0; j < this.keyMap[i].length; j++) {
+          if (!keysArr.includes(this.keyMap[i][j][0])) {
+            keysArr.push(this.keyMap[i][j][0]);
+          }
+        }
+      }
+    }
+    return keysArr;
+  }
 }
 let ht = new HashTable();
 ht.set('maroon', '#800000');
