890d842fb9fbd029e0187a8a0cf81c19aa7392cb
Fernando Cordero <fernando.cordero@externos-cl.cencosud.com>
2021-07-24 09:09:49 -0400

    Graph: depth first iterative

--- dataStructure/graph/depthFirstGraph.js
+++ dataStructure/graph/depthFirstGraph.js
@@ -43,6 +43,28 @@ class Graph {
     })(start);
     return result;
   }
+
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      console.log(stack);
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjancencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 let g = new Graph();
